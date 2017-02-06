545e76f88d1498f9eafb5bd6fa73f066ce47a435
Morrice <morrice_pfeiffer@hotmail.com>
2017-02-06 11:53:26 -0500

    Lesson 4

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+(function () {
+    'use strict';
 
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function ($scope){
+
+$scope.name = "Morrice";
+$scope.sayHello = function (){
+
+    return "Hello Snicker Dooddle";
+}
+
+});
+
+})();
